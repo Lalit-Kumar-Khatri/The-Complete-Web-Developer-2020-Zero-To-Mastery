ad71538d65d7b1148836b0c1a627528b5967b035
Lalit <lalit.khatri83@yahoo.co.in>
2019-12-15 16:38:30 +0530

    Created our own component

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Hello from './Hello';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'));
+ReactDOM.render(<Hello greeting={'hello React ninja'}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

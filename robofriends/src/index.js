551f2eb7c6cff7f58195fd8a1a5167d92fb8cc3f
Lalit <lalit.khatri83@yahoo.co.in>
2019-12-16 14:18:23 +0530

    Created a new component named Card.js, imported that to index.js

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import Card from './Card';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import { robots } from "./robots";
 
-ReactDOM.render(<Hello greeting={'hello React ninja'}/>, document.getElementById('root'));
+ReactDOM.render(
+                <div>
+                    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+                    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+                    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+                </div>
+                , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

b351b8c31d600538c899734f08cf175f8647ac10
Lalit <lalit.khatri83@yahoo.co.in>
2019-12-29 15:26:16 +0530

    Got familiared with more commands like module.exports and require

--- node/script.js
+++ node/script.js
@@ -1,8 +1,6 @@
-const a = 4;
+const script2 =  require ('./script2.js');
+
+const a = script2.largeNumber;
 const b = 5;
 
-setTimeout(()=>{
-    console.log(a + b);
-}, 3000)
-
-console.log(__dirname);
+console.log(a + b);
---
+++ node/script2.js
@@ -0,0 +1,6 @@
+const largeNumber = 356;
+
+
+module.exports = {
+    largeNumber: largeNumber
+};

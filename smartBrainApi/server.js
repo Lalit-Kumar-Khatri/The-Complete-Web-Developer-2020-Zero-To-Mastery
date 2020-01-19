fec66e6ae0d2600699966d367f10de0cbf4a7fb1
Lalit-Kumar-Khatri <lalit.khatri83@yahoo.co.in>
2020-01-19 16:07:46 +0530

    Extracting users through id from database

--- smartBrainApi/server.js
+++ smartBrainApi/server.js
@@ -83,16 +83,17 @@ app.post('/register', (req, res) => {
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            return res.json(user);
-        } 
+    db.select('*').from('users').where({id})
+    .then(user => {
+        if (user.length){
+            res.json(user[0]);
+        } else {
+            res.status(400).json("Not found")
+        }
+        
     })
-    if (!found) {
-        res.status(400).json('not found');
-    }
+    .catch(err => res.status(400).json('error getting user'));
+    
 })
 
 app.put('/image', (req, res) => {

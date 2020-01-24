a36475a06c94f417148e016a2789338a4f8321e1
Lalit-Kumar-Khatri <lalit.khatri83@yahoo.co.in>
2020-01-24 16:28:51 +0530

    Optimised our code to secure the API key

--- smartBrainApi/controllers/image.js
+++ smartBrainApi/controllers/image.js
@@ -1,3 +1,19 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '43c8acf7890144e7ac9a5f498e1e5468'
+   });
+
+   const handleApiCall = (req, res) => {
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+      .then(data => {
+          res.json(data);
+      })
+      .catch(err => res.status(400).json('unable to work with API'))
+   }
+   
+
 const handleImage = (req, res, db) => {
     const { id } = req.body;
     db('users').where('id', '=', id)
@@ -10,5 +26,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-    handleImage
+    handleImage,
+    handleApiCall
 }
